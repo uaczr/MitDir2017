4066497c9cdb3940ce939d431bc78083362471ec
Christoph Hellmann <christoph.hellmann@live.de>
2017-01-21 16:14:55 +0100

    Python brain mit Documentation

---
+++ Python-Brain/communicator.py
@@ -0,0 +1,12 @@
+##@package communicator
+#@author Christoph Hellmann
+#@date 21.01.2017
+#@brief  Communication functions
+
+## Communicator Class
+#
+#Used for communication with peripherals via MQTT
+class communicator:
+    ## Constructor
+    def __init__(self):
+        print("Fun")
